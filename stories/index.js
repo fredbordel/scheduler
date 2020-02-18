ba38bdfe63ccfca78e02ee7242ba57c2335fa555
Fred <frederique.bordeleau@gmail.com>
2020-02-17 19:20:05 -0500

    Implementing components: Confirm, status, erros

--- stories/index.js
+++ stories/index.js
@@ -14,7 +14,9 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
-
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -158,6 +160,34 @@ storiesOf("Button", module)
         ))
           .add("Show", () => (
             <Show 
-            
+            student="Lydia Miller-Jones"
+            interviewer={interviewer}
+            onEdit={action("onEdit")}
+            onDelete={action("onDelete")}
             />
-          ))
+        ))
+         .add("Confirm", () => (
+           <Confirm 
+           message="Delete the appointment?"
+           onConfirm={action("onConfirm")}
+           onCancel={action("onCancel")}
+           />
+        ))
+         .add("Status", () => (
+           <Status 
+           message="Deleting"
+           />
+        ))
+        .add("Error Deleting", () => (
+          <Error 
+          message="Could not delete appointment."
+          onClose={action("onClose")}
+          />
+        ))
+        .add("Error Saving", () => (
+          <Error 
+          message="Could not save appointment."
+          onClose={action("onClose")}
+          />
+        ))
+    
